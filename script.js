428d9680a8842bc8866104c7075827283fbda6d6
Habiba Aminu Abu <habibaaminu199@gmail.com>
2025-02-06 16:00:21 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Event listener for the page load
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded and parsed");
-  const addButton = document.getElementById("task-list");
+
+  const addButton = document.getElementById("add-task-btn");
+  console.log(addButton);
+  
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
   function addTask() {
